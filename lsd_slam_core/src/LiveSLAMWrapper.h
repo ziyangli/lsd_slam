93d514fa7ba4ed66a89f2b2c261becd9f54d5711
ziyangli@archer <ziyang.d.lee@gmail.com>
2015-09-13 16:36:03 +0800

    indent in my style

--- lsd_slam_core/src/LiveSLAMWrapper.h
+++ lsd_slam_core/src/LiveSLAMWrapper.h
@@ -1,22 +1,22 @@
 /**
-* This file is part of LSD-SLAM.
-*
-* Copyright 2013 Jakob Engel <engelj at in dot tum dot de> (Technical University of Munich)
-* For more information see <http://vision.in.tum.de/lsdslam> 
-*
-* LSD-SLAM is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* LSD-SLAM is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with LSD-SLAM. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of LSD-SLAM.
+ *
+ * Copyright 2013 Jakob Engel <engelj at in dot tum dot de> (Technical University of Munich)
+ * For more information see <http://vision.in.tum.de/lsdslam>
+ *
+ * LSD-SLAM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LSD-SLAM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LSD-SLAM. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -30,7 +30,7 @@
 #include "util/SophusUtil.h"
 
 namespace cv {
-	class Mat;
+class Mat;
 }
 
 
@@ -46,55 +46,55 @@ class Output3DWrapper;
 
 struct LiveSLAMWrapper : public Notifiable
 {
-friend class LiveSLAMWrapperROS;
-public:
-	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  friend class LiveSLAMWrapperROS;
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-	LiveSLAMWrapper(InputImageStream* imageStream, Output3DWrapper* outputWrapper);
+  LiveSLAMWrapper(InputImageStream* imageStream, Output3DWrapper* outputWrapper);
 
-	/** Destructor. */
-	~LiveSLAMWrapper();
-	
-	
-	/** Runs the main processing loop. Will never return. */
-	void Loop();
-	
-	/** Requests a reset from a different thread. */
-	void requestReset();
-	
-	/** Resets everything, starting the odometry from the beginning again. */
-	void resetAll();
-
-	/** Callback function for new RGB images. */
-	void newImageCallback(const cv::Mat& img, Timestamp imgTime);
-
-	/** Writes the given time and pose to the outFile. */
-	void logCameraPose(const SE3& camToWorld, double time);
-	
-	
-	inline SlamSystem* getSlamSystem() {return monoOdometry;}
-	
-private:
-	
-	InputImageStream* imageStream;
-	Output3DWrapper* outputWrapper;
-
-	// initialization stuff
-	bool isInitialized;
+  /** Destructor. */
+  ~LiveSLAMWrapper();
 
 
+  /** Runs the main processing loop. Will never return. */
+  void Loop();
 
-	// monoOdometry
-	SlamSystem* monoOdometry;
+  /** Requests a reset from a different thread. */
+  void requestReset();
 
-	std::string outFileName;
-	std::ofstream* outFile;
-	
-	float fx, fy, cx, cy;
-	int width, height;
+  /** Resets everything, starting the odometry from the beginning again. */
+  void resetAll();
+
+  /** Callback function for new RGB images. */
+  void newImageCallback(const cv::Mat& img, Timestamp imgTime);
+
+  /** Writes the given time and pose to the outFile. */
+  void logCameraPose(const SE3& camToWorld, double time);
 
 
-	int imageSeqNumber;
+  inline SlamSystem* getSlamSystem() {return monoOdometry;}
+
+ private:
+
+  InputImageStream* imageStream;
+  Output3DWrapper* outputWrapper;
+
+  // initialization stuff
+  bool isInitialized;
+
+
+
+  // monoOdometry
+  SlamSystem* monoOdometry;
+
+  std::string outFileName;
+  std::ofstream* outFile;
+
+  float fx, fy, cx, cy;
+  int width, height;
+
+
+  int imageSeqNumber;
 
 };
 
--- lsd_slam_core/src/util/settings.h
+++ lsd_slam_core/src/util/settings.h
@@ -1,22 +1,22 @@
 /**
-* This file is part of LSD-SLAM.
-*
-* Copyright 2013 Jakob Engel <engelj at in dot tum dot de> (Technical University of Munich)
-* For more information see <http://vision.in.tum.de/lsdslam> 
-*
-* LSD-SLAM is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* LSD-SLAM is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with LSD-SLAM. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of LSD-SLAM.
+ *
+ * Copyright 2013 Jakob Engel <engelj at in dot tum dot de> (Technical University of Munich)
+ * For more information see <http://vision.in.tum.de/lsdslam>
+ *
+ * LSD-SLAM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LSD-SLAM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LSD-SLAM. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -35,35 +35,35 @@ namespace lsd_slam
 #define UNZERO(val) (val < 0 ? (val > -1e-10 ? -1e-10 : val) : (val < 1e-10 ? 1e-10 : val))
 
 #if defined(ENABLE_SSE)
-	#define USESSE true
+#define USESSE true
 #else
-	#define USESSE false
+#define USESSE false
 #endif
 
 
 #if defined(NDEBUG)
-	#define enablePrintDebugInfo false
+#define enablePrintDebugInfo false
 #else
-	#define enablePrintDebugInfo true
+#define enablePrintDebugInfo true
 #endif
 
 /** ============== constants for validity handeling ======================= */
 
 // validity can take values between 0 and X, where X depends on the abs. gradient at that location:
 // it is calculated as VALIDITY_COUNTER_MAX + (absGrad/255)*VALIDITY_COUNTER_MAX_VARIABLE
-#define VALIDITY_COUNTER_MAX (5.0f)		// validity will never be higher than this
-#define VALIDITY_COUNTER_MAX_VARIABLE (250.0f)		// validity will never be higher than this
+#define VALIDITY_COUNTER_MAX (5.0f)     // validity will never be higher than this
+#define VALIDITY_COUNTER_MAX_VARIABLE (250.0f)      // validity will never be higher than this
 
-#define VALIDITY_COUNTER_INC 5		// validity is increased by this on sucessfull stereo
-#define VALIDITY_COUNTER_DEC 5		// validity is decreased by this on failed stereo
-#define VALIDITY_COUNTER_INITIAL_OBSERVE 5	// initial validity for first observations
+#define VALIDITY_COUNTER_INC 5      // validity is increased by this on sucessfull stereo
+#define VALIDITY_COUNTER_DEC 5      // validity is decreased by this on failed stereo
+#define VALIDITY_COUNTER_INITIAL_OBSERVE 5  // initial validity for first observations
 
 
 #define VAL_SUM_MIN_FOR_CREATE (30) // minimal summed validity over 5x5 region to create a new hypothesis for non-blacklisted pixel (hole-filling)
 #define VAL_SUM_MIN_FOR_KEEP (24) // minimal summed validity over 5x5 region to keep hypothesis (regularization)
 #define VAL_SUM_MIN_FOR_UNBLACKLIST (100) // if summed validity surpasses this, a pixel is un-blacklisted.
 
-#define MIN_BLACKLIST -1	// if blacklist is SMALLER than this, pixel gets ignored. blacklist starts with 0.
+#define MIN_BLACKLIST -1    // if blacklist is SMALLER than this, pixel gets ignored. blacklist starts with 0.
 
 
 
@@ -73,8 +73,8 @@ namespace lsd_slam
 #define FAIL_VAR_INC_FAC 1.1f // after a failed stereo observation, the variance is increased by this factor.
 #define MAX_VAR (0.5f*0.5f) // initial variance on creation - if variance becomes larter than this, hypothesis is removed.
 
-#define VAR_GT_INIT_INITIAL 0.01f*0.01f	// initial variance vor Ground Truth Initialization
-#define VAR_RANDOM_INIT_INITIAL (0.5f*MAX_VAR)	// initial variance vor Random Initialization
+#define VAR_GT_INIT_INITIAL 0.01f*0.01f // initial variance vor Ground Truth Initialization
+#define VAR_RANDOM_INIT_INITIAL (0.5f*MAX_VAR)  // initial variance vor Random Initialization
 
 
 
@@ -86,13 +86,13 @@ namespace lsd_slam
 
 
 #ifdef ANDROID
-	// tracking pyramid levels.
-	#define MAPPING_THREADS 2
-	#define RELOCALIZE_THREADS 4
+// tracking pyramid levels.
+#define MAPPING_THREADS 2
+#define RELOCALIZE_THREADS 4
 #else
-	// tracking pyramid levels.
-	#define MAPPING_THREADS 4
-	#define RELOCALIZE_THREADS 6
+// tracking pyramid levels.
+#define MAPPING_THREADS 4
+#define RELOCALIZE_THREADS 6
 #endif
 
 #define SE3TRACKING_MIN_LEVEL 1
@@ -258,147 +258,147 @@ extern bool fullResetRequested;
 extern bool manualTrackingLossIndicated;
 class RunningStats
 {
-public:
-	int num_stereo_comparisons;
-	int num_stereo_calls;
-	int num_pixelInterpolations;
+ public:
+  int num_stereo_comparisons;
+  int num_stereo_calls;
+  int num_pixelInterpolations;
 
-	int num_stereo_rescale_oob;
-	int num_stereo_inf_oob;
-	int num_stereo_near_oob;
-	int num_stereo_invalid_unclear_winner;
-	int num_stereo_invalid_atEnd;
-	int num_stereo_invalid_inexistantCrossing;
-	int num_stereo_invalid_twoCrossing;
-	int num_stereo_invalid_noCrossing;
-	int num_stereo_invalid_bigErr;
-	int num_stereo_interpPre;
-	int num_stereo_interpPost;
-	int num_stereo_interpNone;
-	int num_stereo_negative;
-	int num_stereo_successfull;
-
-
-	int num_observe_created;
-	int num_observe_blacklisted;
-	int num_observe_updated;
-	int num_observe_skipped_small_epl;
-	int num_observe_skipped_small_epl_grad;
-	int num_observe_skipped_small_epl_angle;
-	int num_observe_transit_finalizing;
-	int num_observe_transit_idle_oob;
-	int num_observe_transit_idle_scale_angle;
-	int num_observe_trans_idle_exhausted;
-	int num_observe_inconsistent_finalizing;
-	int num_observe_inconsistent;
-	int num_observe_notfound_finalizing2;
-	int num_observe_notfound_finalizing;
-	int num_observe_notfound;
-	int num_observe_skip_fail;
-	int num_observe_skip_oob;
-	int num_observe_good;
-	int num_observe_good_finalizing;
-	int num_observe_state_finalizing;
-	int num_observe_state_initializing;
+  int num_stereo_rescale_oob;
+  int num_stereo_inf_oob;
+  int num_stereo_near_oob;
+  int num_stereo_invalid_unclear_winner;
+  int num_stereo_invalid_atEnd;
+  int num_stereo_invalid_inexistantCrossing;
+  int num_stereo_invalid_twoCrossing;
+  int num_stereo_invalid_noCrossing;
+  int num_stereo_invalid_bigErr;
+  int num_stereo_interpPre;
+  int num_stereo_interpPost;
+  int num_stereo_interpNone;
+  int num_stereo_negative;
+  int num_stereo_successfull;
 
 
-	int num_observe_skip_alreadyGood;
-	int num_observe_addSkip;
+  int num_observe_created;
+  int num_observe_blacklisted;
+  int num_observe_updated;
+  int num_observe_skipped_small_epl;
+  int num_observe_skipped_small_epl_grad;
+  int num_observe_skipped_small_epl_angle;
+  int num_observe_transit_finalizing;
+  int num_observe_transit_idle_oob;
+  int num_observe_transit_idle_scale_angle;
+  int num_observe_trans_idle_exhausted;
+  int num_observe_inconsistent_finalizing;
+  int num_observe_inconsistent;
+  int num_observe_notfound_finalizing2;
+  int num_observe_notfound_finalizing;
+  int num_observe_notfound;
+  int num_observe_skip_fail;
+  int num_observe_skip_oob;
+  int num_observe_good;
+  int num_observe_good_finalizing;
+  int num_observe_state_finalizing;
+  int num_observe_state_initializing;
+
+
+  int num_observe_skip_alreadyGood;
+  int num_observe_addSkip;
 
 
 
-	int num_observe_no_grad_removed;
-	int num_observe_no_grad_left;
-	int num_observe_update_attempted;
-	int num_observe_create_attempted;
-	int num_observe_updated_ignored;
-	int num_observe_spread_unsuccessfull;
+  int num_observe_no_grad_removed;
+  int num_observe_no_grad_left;
+  int num_observe_update_attempted;
+  int num_observe_create_attempted;
+  int num_observe_updated_ignored;
+  int num_observe_spread_unsuccessfull;
 
-	int num_prop_removed_out_of_bounds;
-	int num_prop_removed_colorDiff;
-	int num_prop_removed_validity;
-	int num_prop_grad_decreased;
-	int num_prop_color_decreased;
-	int num_prop_attempts;
-	int num_prop_occluded;
-	int num_prop_created;
-	int num_prop_merged;
+  int num_prop_removed_out_of_bounds;
+  int num_prop_removed_colorDiff;
+  int num_prop_removed_validity;
+  int num_prop_grad_decreased;
+  int num_prop_color_decreased;
+  int num_prop_attempts;
+  int num_prop_occluded;
+  int num_prop_created;
+  int num_prop_merged;
 
-	int num_reg_created;
-	int num_reg_smeared;
-	int num_reg_total;
-	int num_reg_deleted_secondary;
-	int num_reg_deleted_occluded;
-	int num_reg_blacklisted;
-	int num_reg_setBlacklisted;
+  int num_reg_created;
+  int num_reg_smeared;
+  int num_reg_total;
+  int num_reg_deleted_secondary;
+  int num_reg_deleted_occluded;
+  int num_reg_blacklisted;
+  int num_reg_setBlacklisted;
 
-	inline RunningStats()
-	{
-		setZero();
-	}
+  inline RunningStats()
+  {
+    setZero();
+  }
 
-	inline void setZero()
-	{
-		memset(this,0,sizeof(RunningStats));
-	}
+  inline void setZero()
+  {
+    memset(this,0,sizeof(RunningStats));
+  }
 
-	inline void add(RunningStats* r)
-	{
-		int* pt = (int*)this;
-		int* pt_r = (int*)r;
-		for(int i=0;i<static_cast<int>(sizeof(RunningStats)/sizeof(int));i++)
-			pt[i] += pt_r[i];
-	}
+  inline void add(RunningStats* r)
+  {
+    int* pt = (int*)this;
+    int* pt_r = (int*)r;
+    for(int i=0;i<static_cast<int>(sizeof(RunningStats)/sizeof(int));i++)
+      pt[i] += pt_r[i];
+  }
 };
 
 
 class DenseDepthTrackerSettings
 {
-public:
-	inline DenseDepthTrackerSettings()
-	{
-		// Set default settings
-		if (PYRAMID_LEVELS > 6)
-			printf("WARNING: Sim3Tracker(): default settings are intended for a maximum of 6 levels!");
+ public:
+  inline DenseDepthTrackerSettings()
+  {
+    // Set default settings
+    if (PYRAMID_LEVELS > 6)
+      printf("WARNING: Sim3Tracker(): default settings are intended for a maximum of 6 levels!");
 
-		lambdaSuccessFac = 0.5f;
-		lambdaFailFac = 2.0f;
+    lambdaSuccessFac = 0.5f;
+    lambdaFailFac = 2.0f;
 
-		const float stepSizeMinc[6] = {1e-8, 1e-8, 1e-8, 1e-8, 1e-8, 1e-8};
-		const int maxIterations[6] = {5, 20, 50, 100, 100, 100};
+    const float stepSizeMinc[6] = {1e-8, 1e-8, 1e-8, 1e-8, 1e-8, 1e-8};
+    const int maxIterations[6] = {5, 20, 50, 100, 100, 100};
 
 
-		for (int level = 0; level < PYRAMID_LEVELS; ++ level)
-		{
-			lambdaInitial[level] = 0;
-			stepSizeMin[level] = stepSizeMinc[level];
-			convergenceEps[level] = 0.999f;
-			maxItsPerLvl[level] = maxIterations[level];
-		}
+    for (int level = 0; level < PYRAMID_LEVELS; ++ level)
+    {
+      lambdaInitial[level] = 0;
+      stepSizeMin[level] = stepSizeMinc[level];
+      convergenceEps[level] = 0.999f;
+      maxItsPerLvl[level] = maxIterations[level];
+    }
 
-		lambdaInitialTestTrack = 0;
-		stepSizeMinTestTrack = 1e-3;
-		convergenceEpsTestTrack = 0.98;
-		maxItsTestTrack = 5;
+    lambdaInitialTestTrack = 0;
+    stepSizeMinTestTrack = 1e-3;
+    convergenceEpsTestTrack = 0.98;
+    maxItsTestTrack = 5;
 
-		var_weight = 1.0;
-		huber_d = 3;
-	}
+    var_weight = 1.0;
+    huber_d = 3;
+  }
 
-	float lambdaSuccessFac;
-	float lambdaFailFac;
-	float lambdaInitial[PYRAMID_LEVELS];
-	float stepSizeMin[PYRAMID_LEVELS];
-	float convergenceEps[PYRAMID_LEVELS];
-	int maxItsPerLvl[PYRAMID_LEVELS];
+  float lambdaSuccessFac;
+  float lambdaFailFac;
+  float lambdaInitial[PYRAMID_LEVELS];
+  float stepSizeMin[PYRAMID_LEVELS];
+  float convergenceEps[PYRAMID_LEVELS];
+  int maxItsPerLvl[PYRAMID_LEVELS];
 
-	float lambdaInitialTestTrack;
-	float stepSizeMinTestTrack;
-	float convergenceEpsTestTrack;
-	float maxItsTestTrack;
+  float lambdaInitialTestTrack;
+  float stepSizeMinTestTrack;
+  float convergenceEpsTestTrack;
+  float maxItsTestTrack;
 
-	float huber_d;
-	float var_weight;
+  float huber_d;
+  float var_weight;
 };
 
 extern RunningStats runningStats;
